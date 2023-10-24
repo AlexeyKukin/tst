892ab0098dcaac15a4e99060a9fff1358040b437
Alexey Kukin <maddog_lex@mail.ru>
2023-10-24 11:50:00 +0300

    init

--- cmd/helloworld/main.go
+++ cmd/helloworld/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 )
 
 func main() {
-	fmt.Println(`
-Alexey Kukin by Golang 2023
-Этот исполняемый файл собирает данные с HW серверов указанных в списке. 
-Так же можно запустить сканирование с ключем \s`)
-	fl := make([]string, 0, 10)
-	fl = flag.Args()
-	fmt.Println(fl)
-
+	fmt.Println("Hello World!")
 }
